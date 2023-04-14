c6fd83aa5c8da71bf3f7e5b6e6ad9ecfe4523cfa
Marcos <itsmarcos.1up@gmail.com>
2023-04-14 11:37:19 -0300

    conexión con mongoDb

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const server = require("./app.js");
+const PORT = process.env.PORT || 3800;
+
+const url = process.env.DB_CREDENTIAL;
+
+//para quitar una alerta molesta de la consola (una actualizacion)
+mongoose.set("strictQuery", false);
+
+mongoose
+    .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("¡Conexión a MongoDB exitosa!");
+        server.listen(PORT, () => {
+            console.log("El servidor está corriendo en: ", PORT);
+        });
+    })
+    .catch((error) => {
+        console.log("Error al conectarse a MongoDB:", error);
+    });
---
+++ src/routes/routes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const router = Router();
+
+// rutas GET
+
+//rutas POST
+
+//rutas PUT
+
+//rutas DELETE
+
+module.exports = router;
